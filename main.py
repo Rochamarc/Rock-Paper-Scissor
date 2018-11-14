1b0b7c4cd8920bb4af98fbf258c830099fb86c57
nuke <rochadcmarcos@gmail.com>
2018-11-14 07:26:27 -0200

    Add points

--- main.py
+++ main.py
@@ -1,23 +1,31 @@
 from kivy.app import App 
 from kivy.uix.boxlayout import BoxLayout
 from random import choice
+from time import sleep
 
 player_points = 0
 pc_points = 0
+match = 1
 
 class Interface (BoxLayout):
 
+    def Wait (self):
+
+        sleep(0.2)
     
     def Rock (self):
 
         global player_points
         global pc_points
+        global match
+        match += 1
 
         player = 'rock'
         computer_choice = ('rock','paper','scissor')
         computer = choice(computer_choice)
         if player == computer:
             
+            self.Wait()
             self.ids.action.text = ''
             self.ids.action.text = 'You Drew'
             
@@ -25,38 +33,45 @@ class Interface (BoxLayout):
         elif player != computer:
             if computer == 'paper':
                 
+                self.Wait()
                 self.ids.action.text = ''
-                self.ids.action.text = 'You lose'
+                self.ids.action.text = 'You Lose'
                 pc_points += 1
 
             else:
                 
+                self.Wait()
                 self.ids.action.text = ''
-                self.ids.action.text = "You win"
+                self.ids.action.text = "You Win"
                 player_points += 1
 
     def Paper (self):
 
         global player_points
         global pc_points
+        global match
+        match += 1
 
         player = 'paper'
         computer_choice = ('rock','paper','scissor')
         computer = choice(computer_choice)
         if player == computer:
             
+            self.Wait()
             self.ids.action.text = ''
             self.ids.action.text = 'You Drew'
 
         elif player != computer:
             if computer == 'rock':
                 
+                self.Wait()
                 self.ids.action.text = ''
                 self.ids.action.text = 'You Win'
                 player_points += 1
 
             else:
                 
+                self.Wait()
                 self.ids.action.text = ''
                 self.ids.action.text = "You Lose"
                 pc_points += 1
@@ -65,24 +80,29 @@ class Interface (BoxLayout):
 
         global player_points
         global pc_points
+        global match 
+        match  += 1
 
         player = 'scissor'
         computer_choice = ('rock','paper','scissor')
         computer = choice(computer_choice)
         if player == computer:
             
+            self.Wait()
             self.ids.action.text = ''
             self.ids.action.text = 'You Drew'
 
         elif player != computer:
             if computer == 'paper':
                 
+                self.Wait()
                 self.ids.action.text = ''
                 self.ids.action.text = 'You Win'
                 player_points += 1
 
             else:
                 
+                self.Wait()
                 self.ids.action.text = ''
                 self.ids.action.text = "You Lose"
                 pc_points += 1
@@ -91,11 +111,9 @@ class Interface (BoxLayout):
 
         global player_points
         global pc_points
+        global match
 
-        player_points_2 = str(player_points)
-        pc_points_2 = str(pc_points)
-
-        self.ids.lbl_points.text = "Your points: %s | Pc points: %s" %(player_points,pc_points) 
+        self.ids.lbl_points.text = "Match: %s | Your points: %s | Pc points: %s" %(match,player_points,pc_points) 
 
 
 
