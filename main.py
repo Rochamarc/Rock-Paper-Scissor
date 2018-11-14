e4e3b0ddf85f28698457d1c8981d3b698ad8714a
nuke <rochadcmarcos@gmail.com>
2018-11-14 07:13:25 -0200

    Add points

--- main.py
+++ main.py
@@ -2,75 +2,100 @@ from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from random import choice
 
+player_points = 0
+pc_points = 0
+
 class Interface (BoxLayout):
 
-    pontuacao = '0'
-
+    
     def Rock (self):
 
-        
-        #self.ids.acao.text = 'Funcionou porra'
+        global player_points
+        global pc_points
+
         player = 'rock'
         computer_choice = ('rock','paper','scissor')
         computer = choice(computer_choice)
         if player == computer:
             
-            self.ids.acao.text = ''
-            self.ids.acao.text = 'You Drew'
+            self.ids.action.text = ''
+            self.ids.action.text = 'You Drew'
             
         
         elif player != computer:
             if computer == 'paper':
                 
-                self.ids.acao.text = ''
-                self.ids.acao.text = 'You lose'
+                self.ids.action.text = ''
+                self.ids.action.text = 'You lose'
+                pc_points += 1
 
             else:
-                self.ids.acao.text = ''
-                self.ids.acao.text = "You win"
-    
+                
+                self.ids.action.text = ''
+                self.ids.action.text = "You win"
+                player_points += 1
+
     def Paper (self):
 
-        #self.ids.acao.text = 'Funcionou porra'
+        global player_points
+        global pc_points
+
         player = 'paper'
         computer_choice = ('rock','paper','scissor')
         computer = choice(computer_choice)
         if player == computer:
             
-            self.ids.acao.text = ''
-            self.ids.acao.text = 'You Drew'
+            self.ids.action.text = ''
+            self.ids.action.text = 'You Drew'
 
         elif player != computer:
             if computer == 'rock':
                 
-                self.ids.acao.text = ''
-                self.ids.acao.text = 'You win'
+                self.ids.action.text = ''
+                self.ids.action.text = 'You Win'
+                player_points += 1
 
             else:
-                self.ids.acao.text = ''
-                self.ids.acao.text = "You lose"
+                
+                self.ids.action.text = ''
+                self.ids.action.text = "You Lose"
+                pc_points += 1
 
     def Scissor (self):
 
-        #self.ids.acao.text = 'Funcionou porra'
+        global player_points
+        global pc_points
+
         player = 'scissor'
         computer_choice = ('rock','paper','scissor')
         computer = choice(computer_choice)
         if player == computer:
             
-            self.ids.acao.text = ''
-            self.ids.acao.text = 'You Drew'
+            self.ids.action.text = ''
+            self.ids.action.text = 'You Drew'
 
         elif player != computer:
             if computer == 'paper':
                 
-                self.ids.acao.text = ''
-                self.ids.acao.text = 'You Won'
+                self.ids.action.text = ''
+                self.ids.action.text = 'You Win'
+                player_points += 1
 
             else:
-                self.ids.acao.text = ''
-                self.ids.acao.text = "You lose"
+                
+                self.ids.action.text = ''
+                self.ids.action.text = "You Lose"
+                pc_points += 1
+    
+    def Points (self):
 
+        global player_points
+        global pc_points
+
+        player_points_2 = str(player_points)
+        pc_points_2 = str(pc_points)
+
+        self.ids.lbl_points.text = "Your points: %s | Pc points: %s" %(player_points,pc_points) 
 
 
 
